75de9d5a94c852ff690bf66804421fef75c0a7c4
unknown <lalwadiyashubham123@gmail.com>
2023-09-04 23:22:40 +0530

    done my all dom 3 assignment

---
+++ dom 3 assignment/prb2/index.js
@@ -0,0 +1,67 @@
+// fill in javascript code here
+
+
+let myform=document.querySelector("form");
+
+
+
+myform.addEventListener('submit',function(e)
+{
+    e.preventDefault();
+    
+
+    let name=document.getElementById("name").value;
+let id=document.getElementById("docID").value;
+let department=document.getElementById("dept").value;
+let exp=document.getElementById("exp").value;
+let email=document.getElementById("email").value;
+let mbl=document.getElementById("mbl").value;
+
+
+let role="";
+
+if(exp>5){
+    role="Senior";
+}
+else if(exp<=5 && exp>=2){
+    role="Junior";
+}
+else{
+    role="Trainee";
+}
+
+let data=[name,id,department,exp,email,mbl,role];
+
+
+let tr =document.createElement("tr");
+let tbody=document.querySelector("tbody");
+   
+    data.forEach(ele=>{
+
+        let td=document.createElement("td");
+        td.innerText=ele;
+
+        tr.appendChild(td);
+    })
+       
+      tbody.appendChild(tr);
+    
+
+      let buttontd=document.createElement("td");
+      let button=document.createElement("button");
+      
+      
+
+      button.innerText="Delete";
+      
+      button.addEventListener("click",function(){
+
+        tr.innerHTML="";
+        // tbody.removeChild(tr);
+      })
+      buttontd.appendChild(button);
+      tr.appendChild(buttontd);
+
+})
+
+
